8283be8db4ddda0ec0daa5c05b3f2ef07b80154f
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2016-02-01 10:27:11 +0100

    import cloog-polylib files
    This commit prepares the reintroduction of PolyLib support.
    
    Polylib support has been dropped since at least version 0.16.0 of Cloog.
    It was available via a separate repository :
    
        http://repo.or.cz/cloog-polylib.git
    
    This commit imports the relevant files from the aforementioned
    repository as they were as of commit
    5d709287f80da4a191e787acde4511db8e9369f4 without any modification.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>
    Signed-off-by: Tobias Grosser <grosser@fim.uni-passau.de>
    Signed-off-by: Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>

---
+++ include/cloog/polylib/cloog.h
@@ -0,0 +1,9 @@
+#ifndef CLOOG_POLYLIB_H
+#define CLOOG_POLYLIB_H
+
+#include <cloog/cloog.h>
+#include <cloog/matrix/constraintset.h>
+#include <cloog/polylib/backend.h>
+#include <cloog/polylib/domain.h>
+
+#endif /* define _H */
